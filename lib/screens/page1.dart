83a06b85da399bcd65bdd0bbf280421a8891c142
Nolan Shrestha <96360893+NolanShrestha@users.noreply.github.com>
2025-01-23 07:29:15 +0545

    Initial Commit

---
+++ lib/screens/page1.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:task4/models/users.dart';
+import 'package:task4/screens/page2.dart';
+import 'package:task4/widgets/BottomNavBar.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Page1(),
+    );
+  }
+}
+
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+  late Future<List<User>> _users;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _users = fetchUsers();
+  }
+
+  Future<List<User>> fetchUsers() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Page2()));
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fetching Users From Network'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<User>>(
+        future: _users,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No users found.'));
+          } else {
+            final users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return ListTile(
+                  onTap: () => print("Pressed"),
+                  title: Text(user.name),
+                  subtitle: Text(user.email),
+                  leading: CircleAvatar(
+                    child: Text(user.name[0]),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          print("Users: ${user.name}");
+                        });
+                      },
+                      icon: const Icon(Icons.favorite_border_rounded)),
+                );
+              },
+            );
+          }
+        },
+      ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/BottomNavBar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const BottomNavBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      margin: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: BottomNavigationBar(
+          elevation: 0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: onItemTapped,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wifi_find_rounded),
+              label: "",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.laptop_chromebook_outlined),
+              label: "",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
